4fa610c06164128bf2a2fa6a03f09249d690c05a
piyush200496 <suyashmishra96@gmail.com>
2017-04-14 18:11:13 +0530

    XML DOM Parser example

--- test.java
+++ test.java
@@ -1,76 +1,32 @@
-package r110214045.piyushmishra;
-import java.util.Scanner;
-import java.sql.*;
-public class test { static String[] d=new String[2];
-	                static String name;
-                    static String pass;
-                    static String repass;
-                    static Scanner r=new Scanner(System.in);
-                    public static void register()
-	{
-		System.out.println("Enter user name");
-		name=r.next();
-		System.out.println("Enter password");
-		pass=r.next();
-		System.out.println("Re-enter password");
-		repass=r.next();
-		boolean valid=false;
-		if(pass.equals(repass))
-		{valid=db.validate(name);
-		 if(!valid)
-			 System.out.println("name exists");
-		 else{
-				d=aesencrypt.x(name,pass);
-				db.insert(d);
-			}
+package org.parser;
+
+import java.io.IOException;
+
+import javax.xml.bind.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class test {
+public static void main(String[] args) throws ParserConfigurationException{
+	try{DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+	DocumentBuilder builder=factory.newDocumentBuilder();
+	Document document=builder.parse("D://sample.xml");
+    org.w3c.dom.Element element=document.getDocumentElement();
+		NodeList list=((Node) element).getChildNodes();
+		for(int i=0;i<list.getLength();i++){
+			Node node=list.item(i);
+			System.out.println(node.getNodeName()+" ");
 		}
-		else{System.out.println("passwords do not match");
-		}
-		
-	}
-                    
-                    
-    public static void login()
-    {System.out.println("Enter user name");
-	name=r.next();
-	System.out.println("Enter password");
-	pass=r.next();
-    d=aesencrypt.x(name,pass);
-    db.check(d);
-    }
-    
-public static void viewuser()
-{ db.fetchuser();
-}
-public static void showencrypt()
-{db.showdb();
+	}catch(ParserConfigurationException| SAXException | IOException e){
+	e.printStackTrace();
 	
 }
-	public static void main(String[] args) throws SQLException {
-		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/demo?useSSL=false","username","password");
-db o=new db(con);
-		int count=0;
-		
-System.out.println("Register");
-System.out.println("Login");
-System.out.println("View Users");
-System.out.println("Show data base state (encrypted)");
-while(true){
-System.out.println("enter option 1-4");
-int option=r.nextInt();
-switch(option){
-case 1: test.register();
-        break;
-case 2: test.login();
-        break;
-case 3: test.viewuser();
-        break;
-case 4: test.showencrypt();
-        break;
-default: System.out.println("wrong option. Enter again");
-       break;
-}// end of switch 
-}//end of while
+	
 }
-
 }
